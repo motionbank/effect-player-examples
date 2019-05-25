ad4acf343deaccb723a35a101fa497c027bdc1ff
Florian Jenett <fjenett@gmail.com>
2019-05-25 12:54:19 +0200

    some style and first version of P5/beginner/001

--- p5-js/beginner/001-simple-dot-trail/script.js
+++ p5-js/beginner/001-simple-dot-trail/script.js
@@ -1,18 +1,48 @@
-let position = {
+/**
+ *  A simple dot and trail example.
+ *
+ *  Use a recent browser with this as we are using ES6+ JavaScript syntax.
+ */
+
+// define a variable to be an array
+// and add first position at 0,0 to it
+let positions = [{
   x: 0,
   y: 0
-}
+}]
 
+// define a variable to set how long the trail will be
+// (in other words: how many positions are kept)
+const maxPositions = 100
+
+// setup drawing area
 function setup() {
+  // the real stage was 8 x 8 meters, values are in millimeters
+  // we draw at 1/10th size (1 pixel == 10 mm)
   createCanvas(800, 800)
 }
 
+// define draw loop
 function draw() {
   background(235)
+
+  // values come in centered in relation to the 8000x8000 stage:
+  // -4000 <--> 4000
+  // so we translate to the center of the drawing area to compensate for that
+  translate(width / 2, height / 2)
+
+  stroke(0)
+  noFill()
+  for (let i = 1, k = positions.length; i < k; i++) {
+    const p1 = positions[i-1]
+    const p2 = positions[i]
+    line(p1.x, p1.y, p2.x, p2.y)
+  }
+
   noStroke()
   fill(0)
-  translate(width / 2, height / 2)
-  ellipse(position.x, position.y, 10, 10)
+  const last = positions.length-1
+  ellipse(positions[last].x, positions[last].y, 10, 10)
 }
 
 const oscPort = new osc.WebSocketPort({
@@ -20,16 +50,19 @@ const oscPort = new osc.WebSocketPort({
   metadata: true
 })
 
-const onWebSocketMessage = function (message) {
-
-  console.log(message)
+const onWebSocketMessage = function(message) {
 
   const x = message.args[0].value / 10
   const y = message.args[2].value / 10
 
-  position = {
-    x, y
+  if (positions.length > maxPositions) {
+    positions = positions.slice(1,maxPositions)
   }
+
+  positions.push({
+    x,
+    y
+  })
 }
 
 oscPort.on('message', onWebSocketMessage)
